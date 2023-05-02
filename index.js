ceedc061d094bfc101523718c274b4d07db1e3df
Aleksandr80 <alexscif2@gmail.com>
2023-05-02 12:37:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.1-9</b>";
+  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.10</b>";
   res.status(200).send(message);
 };
